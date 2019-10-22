b572fc603a996742e3360e1379d1a19c1850757e
bradley <bradley.plater1@gmail.com>
2019-10-22 09:37:24 +0100

    Added deduction functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -12,6 +12,15 @@ class Oystercard
     @balance += amount
   end
 
+  def deduct(amount)
+    raise "Not enough funds" if balance_empty?(amount)
+    @balance -= amount
+  end
+
+  private def balance_empty?(amount)
+    return amount > @balance
+  end
+  
   private def over_limit?(amount)
     return amount > LIMIT
   end
