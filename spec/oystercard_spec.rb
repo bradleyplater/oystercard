c8618266d335db63182ab347f5a7cc0cf291c283
bradley <bradley.plater1@gmail.com>
2019-10-22 09:18:42 +0100

    Created test to check if the balance is able to be topped up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,9 @@ describe Oystercard do
   it "Balance is 0 when initialized" do
     expect(subject.balance).to eq 0
   end
+
+  it "Balance is able to be topped up" do
+    subject.top_up(5)
+    expect(subject.balance).to eq 0
+  end
 end
