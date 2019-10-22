0a801a25e5fda19c4e08db6f0c1732137d62267d
bradley <bradley.plater1@gmail.com>
2019-10-22 09:16:28 +0100

    Created test to check balance is 0 when Oystercard is initialized

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,8 @@ describe Oystercard do
   it "Can create an instance of Oystercard" do
     expect(subject).to be_instance_of(Oystercard)
   end
+
+  it "Balance is 0 when initialized" do
+    expect(subject.balance).to eq 0
+  end
 end
