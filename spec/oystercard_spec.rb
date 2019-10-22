ec40901692a09d5b89053af6ac73359288420c98
bradley <bradley.plater1@gmail.com>
2019-10-22 10:02:14 +0100

    Refactored test to use the begin method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,17 +2,22 @@ require 'oystercard'
 
 describe Oystercard do
   subject { Oystercard.new }
+
+  before(:each) do
+    subject.top_up(Oystercard::LIMIT)
+  end
+
   it "Can create an instance of Oystercard" do
     expect(subject).to be_instance_of(Oystercard)
   end
 
   it "Balance is 0 when initialized" do
-    expect(subject.balance).to eq 0
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq 0
   end
 
   it "Balance is able to be topped up" do
-    subject.top_up(5)
-    expect(subject.balance).to eq 5
+    expect(subject.balance).to eq 90
   end
 
   it "Raises an error when top up is over the limit" do
@@ -20,14 +25,12 @@ describe Oystercard do
   end
 
   it "Balance is able to be deducted" do
-    subject.top_up(10)
     subject.deduct(5)
-    expect(subject.balance).to eq 5
+    expect(subject.balance).to eq 85
   end
 
   it "Does not allow balance to go below 0" do
-    subject.top_up(5)
-    expect { subject.deduct(10) }.to raise_error 'Not enough funds'
+    expect { subject.deduct(100) }.to raise_error 'Not enough funds'
   end
 
   it "Should initially not be in journey" do
