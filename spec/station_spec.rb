6938f225cf777e23133fb32866a741fc6e0028b5
bradley <bradley.plater1@gmail.com>
2019-10-23 11:32:30 +0100

    Test to check if the station class can store

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,9 +1,13 @@
 require 'station'
 
 describe Station do
-  subject { Station.new }
+  subject { Station.new("Station1",1) }
 
   it 'checks that subject is an instance of station' do
     expect(subject).to be_instance_of Station
+  end
+
+  it 'station is able to store a name' do
+    expect(subject.name).to eq "Station1"
   end
 end
